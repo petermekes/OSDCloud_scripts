34817a6ea05c860b1e7bbef361517bc1b838eef2
petermekes <97359990+petermekes@users.noreply.github.com>
2024-03-11 15:12:52 +0100

    Add files via upload

---
+++ LocalAdmin_LAPS.ps1
@@ -0,0 +1,12 @@
+﻿$userName = "wmsadmin"
+$userexist = (Get-LocalUser).Name -Contains $userName
+if($userexist -eq $false) {
+  try{ 
+     New-LocalUser -Name $username -Description "WingMen Solutions local admin account" -NoPassword
+     Exit 0
+   }   
+  Catch {
+     Write-error $_
+     Exit 1
+   }
+}
