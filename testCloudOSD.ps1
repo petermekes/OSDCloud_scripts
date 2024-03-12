d098e04c43f67a1ea31db0720c0ccef0f7df57cb
petermekes <97359990+petermekes@users.noreply.github.com>
2024-03-12 10:38:18 +0100

    Update testCloudOSD.ps1

--- testCloudOSD.ps1
+++ testCloudOSD.ps1
@@ -1,3 +1,12 @@
+#===============================================
+#  Params AutoPilot
+#===============================================
+$grouptag = "AEF-Personal"
+$tenant = 'c3e96bd4-6b32-4bf0-9770-c830983b5d7a'
+$clientid = '41271e57-378c-4210-b274-7c08282f68d2'
+$clientSecret = 'kg88Q~dwe6G0e-NIyvEhKIupdZKlWk36hs43fbkw'
+
+
 #   [PreOS] Update Module
 #================================================
 Write-Host -ForegroundColor Green "Updating OSD PowerShell Module"
@@ -76,22 +85,20 @@ If (!(Test-Path "C:\ProgramData\OSDeploy")) {
 }
 $OOBEDeployJson | Out-File -FilePath "C:\ProgramData\OSDeploy\OSDeploy.OOBEDeploy.json" -Encoding ascii -Force
 
-#================================================
+<#================================================
 #  [PostOS] AutopilotOOBE Configuration Staging
 #================================================
 Write-Host -ForegroundColor Green "Define Computername:"
 $Serial = Get-WmiObject Win32_bios | Select-Object -ExpandProperty SerialNumber
 $TargetComputername = $Serial.Substring(4,4)
 
-$AssignedComputerName = "PeMe-$TargetComputername"
+$AssignedComputerName = "$TargetComputername"
 Write-Host -ForegroundColor Red $AssignedComputerName
 Write-Host ""
 
 Write-Host -ForegroundColor Green "Create C:\ProgramData\OSDeploy\OSDeploy.AutopilotOOBE.json"
 $AutopilotOOBEJson = @"
 {
-    "AssignedComputerName" : "$AssignedComputerName",
-    "AddToGroup":  "grp-autopilot-personal",
     "Assign":  {
                    "IsPresent":  true
                },
@@ -101,12 +108,11 @@ $AutopilotOOBEJson = @"
                    "AssignedUser",
                    "PostAction",
                    "GroupTag",
-                   "Assign"
                ],
     "PostAction":  "Quit",
     "Run":  "NetworkingWireless",
-    "Docs":  "https://google.com/",
-    "Title":  "PeMe Autopilot Register"
+    "Docs":  "https://autopilotoobe.osdeploy.com/",
+    "Title":  "Register in AutoPilot"
 }
 "@
 
@@ -114,6 +120,7 @@ If (!(Test-Path "C:\ProgramData\OSDeploy")) {
     New-Item "C:\ProgramData\OSDeploy" -ItemType Directory -Force | Out-Null
 }
 $AutopilotOOBEJson | Out-File -FilePath "C:\ProgramData\OSDeploy\OSDeploy.AutopilotOOBE.json" -Encoding ascii -Force
+#>
 
 #================================================
 #  [PostOS] OOBEDeploy CMD Command Line - 1.cmd
@@ -124,7 +131,7 @@ PowerShell -NoL -Com Set-ExecutionPolicy RemoteSigned -Force
 Set Path = %PATH%;C:\Program Files\WindowsPowerShell\Scripts
 Start /Wait PowerShell -NoL -C Install-Module OSD -Force -Verbose
 Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/petermekes/OSDCloud_scripts/main/Keyboard.ps1
-Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/petermekes/OSDCloud_scripts/main/Activate.ps1
+Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/AkosBakos/OSDCloud/main/Install-EmbeddedProductKey.ps1
 Start /Wait PowerShell -NoL -C Start-OOBEDeploy
 Start /Wait PowerShell -NoL -C Restart-Computer -Force
 '@
@@ -144,19 +151,23 @@ Start /Wait PowerShell -NoL -C Restart-Computer -Force
 '@
 $OOBECMD | Out-File -FilePath 'C:\Windows\System32\2.cmd' -Encoding ascii -Force
 
+
 #================================================
 #  [PostOS] SetupComplete CMD Command Line
 #================================================
 Write-Host -ForegroundColor Green "Create C:\Windows\Setup\Scripts\SetupComplete.cmd"
-$SetupCompleteCMD = @'
+$SetupCompleteCMD = @"
 RD C:\OSDCloud\OS /S /Q
 XCOPY C:\OSDCloud\ C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\OSD /E /H /C /I /Y
 XCOPY C:\ProgramData\OSDeploy C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\OSD /E /H /C /I /Y
 RD C:\OSDCloud /S /Q
 RD C:\Drivers /S /Q
 RD C:\Temp /S /Q
-'@
+Start /Wait PowerShell -NoL -C Get-WindowsAutoPilotInfo.ps1 -Online -Assign -groupTag $grouptag -TenantId $tenant -AppId $clientid -AppSecret $clientSecret
+"@
+
 $SetupCompleteCMD | Out-File -FilePath 'C:\Windows\Setup\Scripts\SetupComplete.cmd' -Encoding ascii -Force
+
 
 Write-Host ""
 Write-Host "Installing August CU for Autopilot HW hash issues" -ForegroundColor Green 
