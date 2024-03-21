1cbbf4bec7b39b04d05b553d22ca1391cff7abde
petermekes <97359990+petermekes@users.noreply.github.com>
2024-03-21 15:24:57 +0100

    Update hope.ps1

--- hope.ps1
+++ hope.ps1
@@ -39,7 +39,7 @@ function Set-SetupCompleteCreateStartHOPEonUSB {
     Add-Content -path $PSFilePath "Write-Output 'Starting SetupComplete HOPE Script Process'"
     Add-Content -path $PSFilePath "Write-Output 'iex (irm hope.garytown.com)'"
     Add-Content -path $PSFilePath 'iex (irm hope.garytown.com)'
-    Copy-Item "$SetupCompletePath\Setupcomplete.ps1.bak" -Destination "C:\Windows\Setup\Scripts\SetupComplete.ps1" 
+    Copy-Item "$SetupCompletePath\Setupcomplete.ps1.txt" -Destination "C:\Windows\Setup\Scripts\SetupComplete.ps1" 
     #Rename-item $PSFilePath -newname "$PSFilePath.bak1"
     #Rename-item "$PSFilePath.bak" -newname $PSFilePath
 }
