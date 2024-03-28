a1aa79183039d00308e8ccc3c8bf6a66ca20abaa
petermekes <97359990+petermekes@users.noreply.github.com>
2024-03-28 08:40:19 +0100

    Update hope.ps1

--- hope.ps1
+++ hope.ps1
@@ -6,6 +6,10 @@ Creates Setup Complete Files
 Set-ExecutionPolicy Bypass -Force
 iex (irm https://raw.githubusercontent.com/petermekes/OSDCloud_scripts/main/menu.ps1)
 
+Write-Host -Foreground Red $x
+
+pause 
+
 Write-Host -ForegroundColor Green "[+] Function Start-DISMFromOSDCloudUSB"
 Function Test-DISMFromOSDCloudUSB {
     #region Initialize
