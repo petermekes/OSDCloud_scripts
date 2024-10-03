2ff4dd2b467fb8ec587f4b3965f2ef08caffab00
petermekes <97359990+petermekes@users.noreply.github.com>
2024-10-03 20:41:24 +0200

    Update Keyboard.ps1

--- Keyboard.ps1
+++ Keyboard.ps1
@@ -30,13 +30,21 @@ if ($GroupTag -eq 'TF-LU'){$Language = "lb"}
 
 Write-Host -ForegroundColor Green "Set keyboard language to $Language"
 
-$LanguageList = Get-WinUserLanguageList
-$LanguageList.Add("$Language")
+reg add "HKCU\Control Panel\International\User Profile\fr-BE" /v 080C:0000080c /t REG_DWORD /d 1 /f
+reg add "HKCU\Control Panel\International\User Profile" /v InputMethodOverride /t REG_SZ /d "080C:0000080c" /f
+reg delete "HKCU\Control Panel\International\User Profile System Backup\en-US" /va /f
+reg add "HKCU\Control Panel\International\User Profile System Backup\fr-BE" /v 080C:0000080c /t REG_DWORD /d 1 /f
+reg delete "HKCU\Keyboard Layout\Preload" /va /f
+reg add "HKCU\Keyboard Layout\Preload" /v 1 /t REG_SZ /d "0000080c" /f
 
-Set-WinUserLanguageList $LanguageList -Force
 
-$LanguageList = Get-WinUserLanguageList
-$LanguageList.Remove(($LanguageList | Where-Object LanguageTag -like 'en-US'))
-Set-WinUserLanguageList $LanguageList -Force
+#$LanguageList = Get-WinUserLanguageList
+#$LanguageList.Add("$Language")
+
+#Set-WinUserLanguageList $LanguageList -Force
+
+#$LanguageList = Get-WinUserLanguageList
+#$LanguageList.Remove(($LanguageList | Where-Object LanguageTag -like 'en-US'))
+#Set-WinUserLanguageList $LanguageList -Force
 Start-Sleep -Seconds 5
 
