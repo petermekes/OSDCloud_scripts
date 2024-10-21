6eab75d412f8162c5566e4eb2304415c0b809584
petermekes <97359990+petermekes@users.noreply.github.com>
2024-10-21 21:34:37 +0200

    Update hope.ps1

--- hope.ps1
+++ hope.ps1
@@ -30,8 +30,8 @@ if ($env:SystemDrive -eq 'X:') {
 #Variables to define the Windows OS / Edition etc to be applied during OSDCloud
 $Product = (Get-MyComputerProduct)
 $OSVersion = 'Windows 11' #Used to Determine Driver Pack
-$OSReleaseID = '23H2' #Used to Determine Driver Pack
-$OSName = 'Windows 11 23H2 x64'
+$OSReleaseID = '24H2' #Used to Determine Driver Pack
+$OSName = 'Windows 11 24H2 x64'
 $OSEdition = 'Enterprise'
 $OSActivation = 'Volume'
 $OSLanguage = 'en-us'
