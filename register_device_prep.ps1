935472ef2183357749208af68ff4dc7a709b283e
petermekes <97359990+petermekes@users.noreply.github.com>
2024-11-02 23:55:46 +0100

    Create register_device_prep.ps1

---
+++ register_device_prep.ps1
@@ -0,0 +1,16 @@
+# Webhook URL
+$webhook = "https://03ba3236-2802-4923-854b-47bf3e4e203f.webhook.we.azure-automation.net/webhooks?token=M3jirVSnkIgIIUxM0v6n6j4fDs00Yn0c5SDPU%2fc7Y3I%3d"
+
+# Get the computer system and BIOS information
+$computerSystem = Get-CimInstance -ClassName Win32_ComputerSystem
+$bios = Get-CimInstance -ClassName Win32_BIOS
+
+# Create the JSON payload
+$webhookData = @{
+    manufacturer = $computerSystem.Manufacturer
+    model = $computerSystem.Model
+    serialNumber = $bios.SerialNumber
+} | ConvertTo-Json
+
+# Upload the device identity
+Invoke-WebRequest -Method POST -Uri $webhook -Body $webhookData -UseBasicParsing
