67d485aecfc68ef0675b9ee6696d4f61c9b8f410
petermekes <97359990+petermekes@users.noreply.github.com>
2025-01-29 11:26:28 +0100

    Update hope.ps1

--- hope.ps1
+++ hope.ps1
@@ -11,7 +11,7 @@ iex (irm https://raw.githubusercontent.com/petermekes/OSDCloud_scripts/main/menu
 Write-Host -Foreground Red $GroupTag
 sleep -Seconds 3
 
-iex (irm https://raw.githubusercontent.com/petermekes/OSDCloud_scripts/main/functions.ps1)
+#iex (irm https://raw.githubusercontent.com/petermekes/OSDCloud_scripts/main/functions.ps1)
 
 #++++++++++++++++++++++++++++++
 # Functions were here !!
